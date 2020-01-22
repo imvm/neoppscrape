388b0c61c343b9a2ee0dedbf57d8e0c93349aef1
imvm <ianmanor.dev@gmail.com>
2020-01-21 22:04:21 -0300

    Add setup script

---
+++ scripts/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name         = 'bestppscraper',
+    version      = '1.0',
+    packages     = find_packages(),
+    scripts      = ['run.sh'],
+    entry_points = {'scrapy': ['settings = bestppscraper.settings']},
+)
